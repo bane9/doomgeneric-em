ed09f41824430d02cc51d5e9535270f15f13a739
Branislav Brzak <bbranislav99@gmail.com>
2025-02-15 14:04:29 +0100

    Add dummy target

---
+++ doomgeneric/doomgeneric_bare.c
@@ -0,0 +1,43 @@
+#include "doomgeneric.h"
+
+void DG_Init()
+{
+
+}
+
+void DG_DrawFrame()
+{
+
+}
+
+void DG_SleepMs(uint32_t ms)
+{
+
+}
+
+uint32_t DG_GetTicksMs()
+{
+    return 0;
+}
+
+int DG_GetKey(int* pressed, unsigned char* doomKey)
+{
+    return 0;
+}
+
+void DG_SetWindowTitle(const char * title)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    doomgeneric_Create(argc, argv);
+
+    while (1)
+    {
+        doomgeneric_Tick();
+    }
+
+    return 0;
+}
